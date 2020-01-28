1939e6d7480dd2062b50aadd31b9266f044b64d4
Jasper Lankhorst <jasper.lankhorst@gmail.com>
2020-01-28 23:59:10 +0100

    add a setter which makes it sha256.

--- crudApplicationKotlin/src/main/kotlin/com/jasper/crudapplication/person/model/model.kt
+++ crudApplicationKotlin/src/main/kotlin/com/jasper/crudapplication/person/model/model.kt
@@ -1,6 +1,7 @@
 package com.jasper.crudapplication.person.model
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.jasper.crudapplication.sha256
 import java.io.Serializable
 import javax.persistence.Column
 import javax.persistence.Entity
@@ -10,12 +11,18 @@ import javax.persistence.Id
 
 @Entity
 class Person(
-        var password: String = "",
+        pwd: String = "",
         @Column(unique=true) var username: String = "",
         var firstname: String = "",
         var lastname: String = "",
         var description: String? = null,
         @Id @GeneratedValue var id: Long? = null) : Serializable {
+
+    var password: String = pwd
+        //Custom Setter
+        set(value){
+            field = if(value.length > 6) value.sha256() else throw IllegalArgumentException("Password is too small")
+        }
 }
 
 // New data class for incoming user
